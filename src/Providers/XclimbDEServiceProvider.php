e545112cb53d399d69356dd7b1bfad3b4f0bc879
Dima Pyatkovka <dpyatkovka@gmail.com>
2018-07-25 13:56:06 +0200

    change top-nav

--- src/Providers/XclimbDEServiceProvider.php
+++ src/Providers/XclimbDEServiceProvider.php
@@ -27,7 +27,20 @@ class XclimbDEServiceProvider extends ServiceProvider
 
     public function boot(Twig $twig, Dispatcher $dispatcher, ConfigRepository $config)
     {
+	    $dispatcher->listen('IO.init.templates', function (Partial $partial)
+	    {
+		    pluginApp(Container::class)->register('XclimbDE::PageDesign.Partials.Header.NavigationList.twig', NavigationCacheSettings::class);
+		    pluginApp(Container::class)->register('XclimbDE::PageDesign.Partials.Header.SideNavigation.twig', SideNavigationCacheSettings::class);
 
+		    $partial->set('head', 'Ceres::PageDesign.Partials.Head');
+		    $partial->set('header', 'Ceres::PageDesign.Partials.Header.Header');
+		    $partial->set('page-design', 'Ceres::PageDesign.PageDesign');
+		    $partial->set('footer', 'Ceres::PageDesign.Partials.Footer');
+
+		    $partial->set('header', 'XclimbDE::PageDesign.Partials.Header.Header');
+
+		    return false;
+	    }, self::PRIORITY);
     }
 }
 
